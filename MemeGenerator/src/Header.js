cb086ed6ce2e78d016cf2c7adfcd724199c9702f
Gayathri <93761339+Gayastro@users.noreply.github.com>
2021-11-06 13:37:37 +0530

    Create Header.js

---
+++ MemeGenerator/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Header() {
+    return (
+        <div>
+         <img src="https://i.ibb.co/R9926Rc/meme.jpg" alt="meme" border="0"/>
+           
+              </div>
+    );
+    
+}
+
+export default Header;
